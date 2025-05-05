59466c1407c23ddf48bc2feb242c373239bfc142
Kohyuckjoon <akhj123akhj@naver.com>
2025-05-06 01:27:18 +0900

    StopWatch CountDown NumberPicker

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.myapplication
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.myapplication.databinding.ActivityMainBinding
+import com.example.myapplication.databinding.DialogCountdownPickerBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,8 +19,39 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //CountDown Text Click
+        binding.countdownTextView.setOnClickListener {
+            showCountdownDialog()
+        }
+
+
+        //StartButton Click
         binding.startButton.setOnClickListener {
 
         }
+
+
+        //PauseButton Click
+        binding.pauseButton.setOnClickListener {
+
+        }
+    }
+
+    private fun showCountdownDialog () {
+        val dialogBinding = DialogCountdownPickerBinding.inflate(layoutInflater)
+        val numberPicker = dialogBinding.numberPicker
+
+        numberPicker.minValue = 1
+        numberPicker.maxValue = 60
+        numberPicker.value = 10
+
+        AlertDialog.Builder(this)
+            .setTitle("카운트 다운 설정")
+            .setView(dialogBinding.root)
+            .setNegativeButton("취소", null)
+            .setPositiveButton("확인") {_, _->
+                val selectedValue = numberPicker.value
+                binding.countdownTextView.text = selectedValue.toString()
+            }.show()
     }
 }
